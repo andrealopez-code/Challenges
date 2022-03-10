4945b120705d3ce4fb6fee4b95ca7cc8be955e7d
Andrea <missandrealopezortiz@gmail.com>
2022-03-10 14:43:28 -0500

    challenge 4

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ Proceso:
 7. Operar [número1] + [número2].
 8. Dar formato a solo 2 decimales.
 9. Guadrar el resultado anterior en una variable [número3].
-10. Mostrar al usuario el resultado de [número1] + [número2].
+10. Mostrar al usuario el resultado de [número1] + [número2]= [número3].
 
 */
 
@@ -77,4 +77,49 @@ function operationNumber() {
     alert(" El resultado de sumar " + number1 + " y " + number2 + " es = a " + number3);
     
 }
+
+/*
+- Reto #4 “Suma y multiplicación”
+    
+El usuario ingresará 3 números, sumarás los 2 primeros y el resultado será multiplicado por el tercero.
+Añade las consideraciones del punto decimal del reto anterior.
+
+Ejemplo:
+Datos de entrada:2, 3, 4
+Resultado: 20
+
+Entradas: Pedir al usuario que coloque 3 números.
+Salidas: [número1] + [número2] * [número3] = [número4]
+Proceso:
+1. Escribe número1.
+2. Escribe número2.
+3. Escribe número3.
+4. Guardar en una variable [número1].
+5. Guardar en una variable [número2].
+6. Guardar en una variable [número3].
+7. Pasar a [número1] a número.
+8. Pasar a [número2] a número.
+9. Pasar a [número3] a número.
+10. Operar [número1] + [número2] * [número3].
+11. Guardar el resultado en una variable [número4].
+12. Dar formato para 2 decimales [número4].
+13. Mostrar al usuario el resultado de [número1] + [número2] * [número3]= [número4].
+
+*/
+
+function operationNumber2() {
+    let number1, number2, number3, number4;
+    number1= prompt("Hola, Pon un número :)");
+    number2= prompt("Coloca otro número para sumarlo con el anterior");
+    number3= prompt("Y aquí coloca otro número para que se multiplique lo anterior");
+    number1= parseFloat(number1);
+    number2= parseFloat(number2);
+    number3= parseFloat(number3);    
+    number4= (number1 + number2) * number3;
+    number4= number4.toFixed(2);
+    alert("El resutlado de " + number1 + " + " + number2 + " * " + number3 + " es = a " + number4);
+}
+
+/*
+
 
